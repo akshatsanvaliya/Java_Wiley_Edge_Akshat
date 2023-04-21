481d1c9082ec658c7ae62b128476847f8bd16aa2
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-21 11:56:53 +0530

    Add files via upload

--- Methodtyp.java
+++ Methodtyp.java
@@ -1,5 +1,13 @@
 package Wiley_pkg_1;
 
+class test{
+	public static int add(int v1, int v2)
+	{
+		int a = v1+v2;
+//		System.out.println("instance method add: - " + a);
+		return a;
+	}
+}
 public class Methodtyp {
 	public void add(int v1, int v2)
 	{
@@ -25,6 +33,7 @@ public class Methodtyp {
 		
 		int m = mul(23,45);
 		System.out.println("mul in main func: - " + m);
+		System.out.println("access method from another class: -" + (test.add(2, 4)));
 		mt.add(345,6789);
 		mt.div(10,2);
 
