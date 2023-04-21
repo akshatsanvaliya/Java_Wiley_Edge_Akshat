17b61a9669ff5ab4a59b30895d9fff8c251eb82f
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-21 15:29:18 +0530

    Multi Level

---
+++ Multiple_Inherit.java
@@ -0,0 +1,36 @@
+package Wiley_pkg_1;
+
+
+
+ class Vehicle
+ {
+	 public void tire() {
+		 System.out.println("Tire");
+	 }
+	 public void steering() {
+		 System.out.println("steering");
+	 }
+ }
+ 
+ class Car extends Vehicle
+ {
+	 public void airbag() {
+		 System.out.println("airbag");
+	 }
+ }
+ 
+ class SmartCar extends Car
+ {
+	 public void sensor() {
+		 System.out.println("sensor");
+	 }
+ }
+ public class Multiple_Inherit {
+	public static void main(String[] args) {
+		SmartCar sc= new SmartCar();
+		sc.airbag();
+		sc.sensor();
+		sc.steering();
+	}
+
+}
