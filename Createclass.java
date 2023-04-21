87b935c946a51d533f8f0425081ca1ed29907b42
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-21 10:42:13 +0530

    Playing with Classes

--- Createclass.java
+++ Createclass.java
@@ -1,24 +1,34 @@
 package Wiley_pkg_1;
 
-public class Createclass {
+public class Createclass implements Cloneable {
 
 int  a = 10;
 public void add() {
-	System.out.println("Number Adding: ");
+	int a= 10;
+	int b= 10;
+	int c=a+b;
+	System.out.println("Number Adding: " + c);
 }
 public static void main(String args[]) throws Exception {
+	//create object using new keyword
 	Createclass c= new Createclass();
 	c.add();
-	System.out.println("Object: -" + c);
+	System.out.println("create object using new keyword-\n" + c);
+	
+	//create object using newInstance() method
 	try {
 		Class cc = Class.forName("Wiley_pkg_1.Createclass");
 		Createclass ci=  (Createclass) cc.newInstance();
 		ci.add();
-		System.out.println("Inside Createclass Class: - "+ ci);
+		System.out.println("create object using newInstance() method - \n"+ ci);
 	} catch (ClassNotFoundException e) {
 System.out.println("Exception Orrured: - " + e);		
 e.printStackTrace();
 	}
+	// object created using Cloneable(objClone)
+	Createclass objClone = (Createclass) c.clone();
+	objClone.add();
+	System.out.println("object created using Cloneable(objClone) \n" + objClone + "\n"+ c);
 }
 	}
 
