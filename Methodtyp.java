35c15ca635f8c47764aaf89ccfed323b42ac0063
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-21 11:49:53 +0530

    Add files via upload

--- Methodtyp.java
+++ Methodtyp.java
@@ -4,23 +4,29 @@ public class Methodtyp {
 	public void add(int v1, int v2)
 	{
 		int a = v1+v2;
-		System.out.println("add: - " + a);
+		System.out.println("instance method add: - " + a);
 	}
-	public static void mul(int v1, int v2)
+	public static int mul(int v1, int v2)
 	{
 		int a = v1*v2;
-		System.out.println("mul: - " + a);
+		//System.out.println(" static mul: - " + a);
+		return a	;
 	}
 	public void div(int v1, int v2)
 	{
-		int a = v1%v2;
-		System.out.println("div: - " + a);
+		int a = v1/v2;
+
+		System.out.println(" instance method div: - " + a);
+		this.mul(v1, v2);
+
 	}
 	public static void main(String[] args) {
-		Methodtyp mt= new Methodtyp();	
-		mul(23,45);
+		Methodtyp mt= new Methodtyp();
+		
+		int m = mul(23,45);
+		System.out.println("mul in main func: - " + m);
 		mt.add(345,6789);
-		mt.div(34,2);
+		mt.div(10,2);
 
 	}
 
