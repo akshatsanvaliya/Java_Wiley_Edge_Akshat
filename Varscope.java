3a97677b00fe42bbe67a589f45ff2869001ed92a
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-21 11:29:48 +0530

    Local and Global Variable

--- Varscope.java
+++ Varscope.java
@@ -2,11 +2,28 @@ package Wiley_pkg_1;
 
 public class Varscope {
 	int a=10;	
+	static String name= "Akshat";
+	int val = 12;
+	int val1= 25;
+
+	void add()
+	{
+		int val =10;
+		int val1= 25;
+		//	int val2= val+val1;
+		//	System.out.println("Add: -" + val2);
+		System.out.println("Addition: - " + (this.val+this.val1));
+	}
 	public static void main(String[] args) {
-	int b=10;
-	Varscope vs = new Varscope();
-System.out.println("Outside method (Global): - " + vs.a);
-System.out.println("Inside method (Local): -" + b);
+		int b=10;
+		int val = 100;
+
+		Varscope vs = new Varscope();
+		vs.add();
+		System.out.println("Declare both side: -" + val);
+		System.out.println("Name: -" + name);
+		System.out.println("Outside method (Global): - " + vs.a);
+		System.out.println("Inside method (Local): -" + b);
 	}
 
 }
