6ba3a3d5994165ef08a25acc2d2f3ecf3d0384fb
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-24 12:48:43 +0530

    Add files via upload

--- ExceptionHandle.java
+++ ExceptionHandle.java
@@ -1,5 +1,5 @@
 package Wiley_pkg_1;
-import java.lang.*;
+
 public class ExceptionHandle extends Throwable {
 
 	public static void main(String[] args) {
@@ -9,12 +9,20 @@ int arr[]= {1,2,3,4};
 try {
 	System.out.println(arr[1]);
 	System.out.println(arr[47]);	
+	System.out.println(12/0);
 }
-catch (Exception e) {
+catch (ArithmeticException e) {
+	System.out.println("Catch Bock 1");
 	System.out.println("Exception is: "+e);
 	System.out.println(e.getMessage());
 	System.out.println(e.toString());
 	e.printStackTrace();
+}
+catch (ArrayIndexOutOfBoundsException ae) {
+	System.out.println("Catch Block 2");
+}
+catch (Exception e) {
+	System.out.println("Exception: " + e);
 }
 System.out.println("another statem");
 
