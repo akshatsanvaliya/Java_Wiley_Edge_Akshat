ea7f36ec1dc58b2bf6511287df780b316f2874bf
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-24 14:50:38 +0530

    added nested try-catch

--- FinallyBlck.java
+++ FinallyBlck.java
@@ -8,15 +8,23 @@ try {
 	
 	System.out.println("Akshat");
 
-	//System.out.println(10/0);
-	System.exit(0);
+	System.out.println(10/0);
+	//System.exit(0);
 	System.out.println("Sanvaliya");
 }
 catch (Exception e) {
 		System.out.println("Catch block");
-		System.out.println(10/0);
+		//System.out.println(10/0);
+		try {
+			System.out.println("try catch block inside try catch block" );
+			System.out.println(10/0);
+			
+		}
+		catch (Exception ee) {
+System.err.println(ee);		}
 }
 finally {
+	//System.out.println(10/0);
 	System.out.println("Clone Connection");	
 }
 	}
