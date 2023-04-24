44374274dba9a615174833a5a7d69b27841cc068
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-24 09:49:21 +0530

    abstract updated

--- AbstractClass.java
+++ AbstractClass.java
@@ -6,7 +6,12 @@ abstract class Lol{ // must declare the class abstract in order to create abstra
 	public void m2()
 	{
 		System.out.println("Inside Lol class and m2 method");
-	}public static void main(String[] args) {
+		
+	}
+	static {
+		System.out.println("Inside Lol's abstract class's static");
+	}
+	public static void main(String[] args) {
 		
 		System.out.println("main method define insode abstract class");
 	}
@@ -23,15 +28,19 @@ abstract class Lol{ // must declare the class abstract in order to create abstra
 		System.out.println("Lol1\'s constructor");
 	}
 	
+	static {
+		System.out.println("Inside Lol1 class's static");
+	}
 }
  
 
 public class AbstractClass {
 
 	public static void main(String[] args) {
-		Lol1 t2= new Lol1();
-		t2.m1();
-		t2.m2();
+		//Lol1 t2= new Lol1();
+		Lol1 l1= new Lol1();
+		l1.m1();
+		l1.m2();
 		Lol.main(args);
 	}
 
