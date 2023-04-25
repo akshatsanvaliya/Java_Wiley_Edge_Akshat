19bdb4eb5f836ff064a0177d72335cf245402bc0
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-25 10:20:36 +0530

    String Buffer

---
+++ StringBUff.java
@@ -0,0 +1,13 @@
+package String;
+
+public class StringBUff {
+
+	public static void main(String[] args) {
+StringBuffer sb= new StringBuffer("Akshat");
+StringBuffer sb1= new StringBuffer("Akshat");
+StringBuffer sb2= new StringBuffer("Akshat");
+System.out.println(sb.equals(sb2));
+System.out.println(sb);
+	}
+
+}
