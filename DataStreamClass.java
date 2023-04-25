aaf5db0a2b28bcc53c3be640c2d539792368f2cc
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-25 15:17:08 +0530

    Input/Output Stream

--- DataStreamClass.java
+++ DataStreamClass.java
@@ -1,6 +1,8 @@
 package IOStream;
 
 import java.io.BufferedInputStream;
+import java.io.DataInput;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -13,6 +15,22 @@ public class DataStreamClass {
 		df.writeBytes("I am Ironman🫰");
 		df.flush();
 		df.close();
+		
+		
+		FileInputStream fss = new  FileInputStream("C:\\Users\\Admin\\Documents\\test1.txt");
+		DataInputStream  dff = new  DataInputStream(fss);
+		int count = fss.available();
+		byte[] ba= new byte[count];
+		
+		dff.read(ba);
+		for(byte b: ba) {
+			System.out.println((char)b);
+		}
+		
+		fss.close();
+		
+		dff.close();
 	}
 
+	
 }
