5e71c02c1a360b3455e8de5ea948ab89d83038bf
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-25 11:56:15 +0530

    Add files via upload

---
+++ StringFormat.java
@@ -0,0 +1,12 @@
+package String;
+
+public class StringFormat {
+
+	public static void main(String[] args) {
+
+String title = "Java Dev";
+float salary = 2000.36f;
+System.out.format("%s has %.2f USD salary", title, salary);
+	}
+
+}
