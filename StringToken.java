e447838b4c615460ae6b9452f2416ffbdcc4adbe
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-25 11:43:29 +0530

    Add files via upload

--- StringToken.java
+++ StringToken.java
@@ -7,12 +7,12 @@ public class StringToken {
 	public static void main(String[] args) {
 		
 		String input = "Hey there, How's the day goin!";
-		StringTokenizer st = new StringTokenizer(input, " ");
+		StringTokenizer st = new StringTokenizer(input, ",");
 		
-		System.out.println("count tokens: "+ st.countTokens());
+		System.out.println("count tokens:"+ st.countTokens());
 		
 		while(st.hasMoreElements()) {
-			System.out.println("Inside while: "+st.nextToken());
+			System.out.println("Inside while:"+st.nextToken().trim());
 			
 		}
 		//System.out.println(st.nextToken());
