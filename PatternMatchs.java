ae9c67c0e50bc0798b15487ad31b74425c628dbc
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-27 10:25:09 +0530

    RegEX

---
+++ PatternMatchs.java
@@ -0,0 +1,18 @@
+package Random;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatchs {
+
+	public static void main(String[] args) {
+
+		Pattern pattern = Pattern.compile("agent \\d{3,4}");
+		Matcher matcher = pattern.matcher("agent 007");
+		boolean found = matcher.find();
+		boolean match = matcher.matches();
+		System.out.println("Found: " + found);
+		System.out.println("Match: " +match);
+	}
+
+}
