b796e5925c21bb55dbd04850b171b7cf32b54b8b
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-27 15:26:43 +0530

    Threads

--- ThreadPriority.java
+++ ThreadPriority.java
@@ -1,23 +1,28 @@
 package Thread;
 
-	class TopPriority extends Thread{
-		public void run() {
-			for (int i=1; i<=10; i++) {
-				System.out.println(
-						"Thread Name: -        " +Thread.currentThread().getName() 
-						+ "\nThread Priority: =  " +Thread.currentThread().getPriority() 
-						);
-			}
+class TopPriority extends Thread {
+	public void run() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Thread Name: -        " + Thread.currentThread().getName() + "\nThread Priority: =  "
+					+ Thread.currentThread().getPriority()
+
+			);
 		}
 	}
+}
 
 public final class ThreadPriority {
 
 	public static void main(String[] args) {
-		TopPriority tp = new  TopPriority();
+		
+		System.out.println("Main Thread: - " + Thread.currentThread().getPriority());
+		TopPriority tp = new TopPriority();
 		TopPriority tp2 = new TopPriority();
+//		TopPriority tp3 = new TopPriority();
 		tp.start();
 		tp2.start();
+		tp.setPriority(10);
+//		tp3.start();
 	}
 
 }
