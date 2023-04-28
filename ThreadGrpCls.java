347edccdd08ff33eb5b668ff2b3e1dc9d1445805
Akshat Sanvaliya <55980380+akshatsanvaliya@users.noreply.github.com>
2023-04-28 10:08:59 +0530

    Thread Group Class

---
+++ ThreadGrpCls.java
@@ -0,0 +1,25 @@
+package Thread;
+
+class Demo implements Runnable{
+	public void run() {
+		System.out.println("Thread Name: "+ Thread.currentThread().getName());
+	}
+}
+public class ThreadGrpCls {
+
+	public static void main(String[] args) {
+ThreadGroup tg = new ThreadGroup("Parent Group");
+ThreadGroup tg2= new ThreadGroup(tg, "Child Grp");
+
+Thread t= new Thread(tg, new Demo(),"First");
+Thread t2= new Thread(tg, new Demo(),"First");
+Thread t3= new Thread(tg, new Demo(),"First");
+
+t.start();
+t2.start();
+t3.start();
+
+System.out.println("Active Group of threads: "+ tg.activeCount());
+	}
+
+}
